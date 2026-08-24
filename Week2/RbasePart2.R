22fc1f90ea57c776d35c0503ca1cb77dc8189d18
Jessica-Crystal <jessica.ranto@icloud.com>
2026-08-24 13:10:17 -0400

    add R base part II

---
+++ Week2/RbasePart2.R
@@ -0,0 +1,50 @@
+# my code
+n <- 0:10
+Rmat <- rbind(0.5^n, 0.5^(n-1), 0.5^(n-2) )
+Rmat
+
+# Teacher's code
+n <- 10
+M1 <- matrix(rep(0:(n-1), each=n), n, n) # replicate from zero to 9
+M2 <- matrix(rep(0:(n-1),n), n, n)
+0.5^abs(M1 - M2)
+
+
+#the apply function 
+x <- matrix(1:6, nrow=2, ncol=3)
+apply(x, 1, mean) # apply on the row part and computing the mean for each row
+apply(x, 2, mean) # apply on each column, ie calculating the mean on each column
+
+# question for "apply" function
+sample <- runif(n=100, 0, 1)
+mean(sample)
+
+# teacher's code
+n <- 100
+s <- 1000
+mean(runif(n))
+x <- matrix(runif(n*s), nrow=n, ncol=s)
+xmean <- apply(x, 2, mean)
+hist(xmean)
+
+
+# Build function
+
+x <- c(2, -3, 0, 8)
+y <- c(1, 5, 2, 0)
+sum_x <- sum(x)
+csum_x <- cumsum(x)
+xmax <- max(x)
+pmax(x, y) # compare between x and y and take the max, pairwise max
+
+# practice question
+n <- 1000
+z <- runif(n)
+plot(cumsum(z)/n)
+
+#teacher's code
+n <- 1000
+z <- runif(n)
+ind <- 1:n
+cbind(cumsum(z), 1:n)
+plot(cumsum(z)/ind)
