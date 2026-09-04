11edba8025a9e240e5f0b65c3fa253f52ce00dfb
Jessica-Crystal <jessica.ranto@icloud.com>
2026-09-04 10:11:59 -0400

    Update howmework 2 question 3

--- Week3/Homework2.R
+++ Week3/Homework2.R
@@ -4,11 +4,10 @@
 Bschool <- read.csv("Dataset\\Bschool.csv")
 
 #1. Which professor rank (i.e., associate, assistant, or professor) has the highest average teaching evaluation score?
-  p1 <- mean(Bschool$score[Bschool$title == "Associate Professor"])
-  p2 <- mean(Bschool$score[Bschool$title == "AssistantProfessor"])
-  p3 <- mean(Bschool$score[Bschool$title == "Professor"])
-  highest <- max(p1, p2, p3)
-  cat("The professor who has the higher rank", highest)
+
+mean_score <- tapply(Bschool$score, Bschool$title, mean)
+highest <- max(mean_score)
+cat("The professor who has the higher rank", highest)
 #2. What is the number of female associate professors? 
   
 female_associate_professors <- sum(Bschool$title =="Associate Professor" &
@@ -19,6 +18,6 @@ cat("The number of female associates professor:", female_associate_professors)
 # No, the median teaching evaluation score in fall semester is lower than the spring semester
 eva_fall <- median(Bschool$score[Bschool$semester == "fall"])
 eva_spring <- median(Bschool$score[Bschool$semester == "spring"])
-max(eva_fall, eva_spring)
+eva_fall >=  # this return FALSE, so the answer is nO§
 cat("The median score that is high is in the spring", eva_spring)
 
