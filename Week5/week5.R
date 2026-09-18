b43eb4128c4d8b8352b64cc9ab74c1f5e8d61bfc
Jessica-Crystal <jessica.ranto@icloud.com>
2026-09-18 13:07:11 -0400

    update hw4 and dataset

--- Week5/week5.R
+++ Week5/week5.R
@@ -132,3 +132,116 @@ airports
 # missing values
 # time and dates
 # multiples date
+
+## 18th september 2026. Last topic on the data handling
+glimpse(flights) # gives the glimpse of the flights
+# average arr_delay each day
+flights |> 
+  group_by(month, day) |>
+  summarize(
+    avg_arr_delay = mean(arr_delay)
+  )
+summary(flights) # to many NA values 
+
+flights |> 
+  group_by(month, day) |>
+  summarize(
+    avg_arr_delay = mean(arr_delay,
+                         na.rm = TRUE)
+  )
+# IN THE EXAM? there might be NA values, so need to remove them before calculation
+
+
+flights |> 
+  group_by(month, day) |>
+  summarize(
+    avg_arr_delay = mean(arr_delay,
+                         na.rm = TRUE),
+    .groups = "drop"
+  ) # remove the groups:month
+
+## replace the missing values: fix the missing values
+
+flights |> 
+  group_by(month, day) |>
+  mutate(
+    arr_delay_na_rm = replace_na(
+      arr_delay, mean(arr_delay, na.rm=TRUE)
+    )
+  ) # be careful using it
+
+## illegal computation and numerical issue
+# example
+sqrt(-1)
+
+## empty group
+health <- tibble(
+  id = 1:5,
+  smoker = rep("No", 5)
+)
+
+table(health$smoker)|> barplot()
+health1 <- tibble(
+  id = 1:5,
+  smoker = factor(rep("No", 5), levels = c("Yes", "No")
+)
+)
+table(health1$smoker)|> barplot()
+####################################################"
+flights
+date_time_now <- now()
+ymd("2017-01-31")
+mdy("January 31st, 2017")
+dmy("31-Jan-2017")
+ymd_hms("2017-01-31 20:11:59")
+mdy_hm("01/31/2017 08:01")
+ymd("2017-01-31", tz="UTC")
+wday("2017-01-31", label=TRUE)
+a <- ymd("2017-01-31") - ymd("2016-01-31")
+a/365
+b <- ymd("2017-01-31") - ymd("2015-01-31")
+a>b
+time_length(interval(("2016-01-31"),ymd("2017-01-31")), unit="years")
+time_length(interval(("2016-01-31"),ymd("2017-01-31")), unit="days")
+
+############################
+time_delay <- flights |> 
+  select(year, month, day, hour, minute, arr_delay)|>
+  mutate(
+    departure = make_datetime(year, month, day, hour),
+    weekday = wday(departure, label=TRUE),
+    week = floor_date(departure, unit="week")
+      
+    ) # foor_date means first day of the week
+  
+time_delay |> 
+  group_by(departure)|>
+  summarize(
+    mean_delay = mean(arr_delay, na.rm=TRUE)
+  )|>
+  ggplot(
+    aes(x=departure, y=mean_delay)
+  )+ 
+  geom_line()
+
+time_delay |> 
+  group_by(week)|>
+  summarize(
+    mean_delay = mean(arr_delay, na.rm=TRUE)
+  )|>
+  ggplot(
+    aes(x=week, y=mean_delay)
+  )+ 
+  geom_line()
+###################################################
+# new functions: ceiling_date, round_date
+
+time_delay |> 
+  group_by(weekday) |>
+  summarize(
+    mean_delay = mean(arr_delay, na.rm=TRUE)
+  ) |>
+  ggplot(
+    aes(x = weekday, y=mean_delay)
+  ) + 
+  geom_bar(stat = "identity")
